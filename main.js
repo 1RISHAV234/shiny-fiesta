37fe590c9237411d80338747116e0e74c85951f5
1RISHAV234 <83695942+1RISHAV234@users.noreply.github.com>
2021-05-10 16:17:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function ESLINTI() {
+    "use strict";
+    window.alert("My hobby is to do some maths sums everyday in a free time. It easily passes my free time and even enriches my knowledge. It even helps for me to calculate the questions fast and may help me in the future. As being a 11 year old, those are simple questions for us and the complex maths start from 8th class But, I try to reach my goal for the class 6 level :) But, Thank you for reading my hobby 😀");
+}
+function idk() {
+    "use strict";
+    window.alert("Hi my name is RISHAV and my current role is to study and to beat the exams. As wish my ultimate goal is to become a software engineer. My biggest achievement is that I still come 3-5 position in my class and 8-10 in section with 92 percentage...");
+}
+function moreiq() {
+    "use strict";
+    window.alert("As from 2021, I started making my own website with Brackets and Visual Studio Code with an html, css and js. Thank you for reading this website :). By the way, Please forgive me if there are any typo errors but still I tried to avoid mistakes 😀");
+}
